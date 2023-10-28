a2bd9484469e018a295372fd687fa5708df808b3
Egor <eockunev@yandex.ru>
2023-10-28 11:14:34 +0700

    day 4

--- scripts/index.js
+++ scripts/index.js
@@ -6,77 +6,146 @@ async function readTextFile(fileUrl) {
         return jsonFile;
     }
 }
-async function createFormAddPost(){
-    const text = await readTextFile("./schemas/addpost.json");
-    let AddPost = JSON.parse(text);
-    let addPostBlock = document.querySelector('.addpost');
-    const form = document.createElement('form');
-    form.setAttribute('name', 'addpost');
-    form.setAttribute('id', 'addpost');
+function createInputs(form, inputData) {
+    let input;
+    switch (inputData.input.type) {
+        case 'file':
+            input = fileInput(form, inputData);
+            break;
+        case 'technology':
+            input = technologyInputs(form, inputData);
+            break;
+        case 'color':
+            colorInputs(form, inputData);
+            break;
+        case 'checkbox':
+            input = checkboxInput(form, inputData);
+            break;
+        case 'textarea':
+            input = textareaInput(form, inputData);
+            break;
+        default:
+            input = simpleInput(form, inputData);
+            break;
+    }
+    if (inputData.input.required) { input.setAttribute('required', inputData.input.required ) }
 
-    AddPost.fields.forEach((inputData) => {
-        let input;
-        if (inputData.input.type === 'textarea') {
-            input = document.createElement(inputData.input.type);
-            input.setAttribute('rows', '2' );
-        } else { input = document.createElement('input'); }
+
+    //===========================================
+    let divCheckbox;
+    if (inputData.input.type === 'textarea') {
+        input = document.createElement(inputData.input.type);
+        input.setAttribute('rows', '2' );
+    } else if(inputData.input.type === 'checkbox') {
+        input = document.createElement('input');
+        divCheckbox = document.createElement('div');
+    } else if(inputData.input.type === 'color') {
+        let type = "colors";
+        inputData.input[type].forEach((color) =>{
+            let checkBoxColor = document.createElement('input');
+            let labelBlockColor = document.createElement('label');
+            let divColors = document.createElement('div');
+            checkBoxColor.setAttribute('type', 'checkbox');
+            checkBoxColor.setAttribute('id', color);
+            checkBoxColor.setAttribute('name', color);
+            labelBlockColor.setAttribute('for', color);
+            labelBlockColor.setAttribute('style', `background-color: ${color}`)
+            divColors.classList.add('color-block');
+            divColors.appendChild(checkBoxColor);
+            divColors.appendChild(labelBlockColor);
+            form.appendChild(divColors);
+        });
+    } else {
+        input = document.createElement('input');
+    }
+    if(inputData.input.type !== 'color' && inputData.input.type !== 'technology') {
         input.setAttribute('type', inputData.input.type );
         input.setAttribute('name', inputData.label );
         input.setAttribute('id', inputData.label );
-        if (inputData.input.required) { input.setAttribute('required', inputData.input.required ) }
-        let inputLabel = document.createElement('label');
-        inputLabel.innerHTML = inputData.label;
-        inputLabel.setAttribute('for', inputData.label );
+    }
+    let inputLabel = document.createElement('label');
+    inputLabel.innerHTML = inputData.label;
+    inputLabel.setAttribute('for', inputData.label );
+    if (inputData.input.type === 'checkbox') {
+        divCheckbox.classList.add('checkbox-block')
+        divCheckbox.appendChild(input);
+        divCheckbox.appendChild(inputLabel);
+        form.appendChild(divCheckbox);
+    } else if(inputData.input.type === 'color') {
+    } else {
         form.appendChild(inputLabel);
         form.appendChild(input);
-    });
-
-    let checkboxData = AddPost.references[0];
+    }
+}
+function createReference(form, formText) {
+    let checkboxData = formText.references[0];
     let checkbox = document.createElement('input');
     checkbox.setAttribute('type', checkboxData.input.type );
     checkbox.setAttribute('id', 'viewauthor');
     checkbox.setAttribute('name', 'viewauthor');
     checkbox.required = checkboxData.input.required;
     checkbox.checked = checkboxData.input.checked !== "false";
-    let labelCheckboxData = AddPost.references[1];
+    let labelCheckboxData = formText.references[1];
     let labelCheckbox = document.createElement('label');
     labelCheckbox.setAttribute('for', checkbox.getAttribute('id'));
     let labelLink = document.createElement('a');
     labelLink.setAttribute('href', labelCheckboxData.ref);
     labelLink.innerHTML = labelCheckboxData.text;
-    labelCheckbox.innerHTML = labelCheckboxData["text without ref"];
+    labelCheckbox.innerHTML = labelCheckboxData["text without ref"] + " ";
     labelCheckbox.appendChild(labelLink);
-
-    let buttonData = AddPost.buttons[0];
+    let divCheckbox = document.createElement('div');
+    divCheckbox.classList.add('checkbox-block')
+    divCheckbox.appendChild(checkbox);
+    divCheckbox.appendChild(labelCheckbox);
+    form.appendChild(divCheckbox);
+}
+function createButton(form, formText) {
+    let buttonData = formText.buttons[0];
     let button = document.createElement('input');
     button.setAttribute('type', 'submit')
     button.setAttribute('value', buttonData.text);
-
-    form.appendChild(checkbox);
-    form.appendChild(labelCheckbox);
     form.appendChild(button);
-    addPostBlock.appendChild(form);
+}
+async function createForm(formText){
+    let formBlock = document.querySelector('.form-block');
+    const form = document.createElement('form');
+    form.setAttribute('name', 'form');
+    form.setAttribute('id', 'form');
+
+    formText.fields.forEach((inputData) => {
+        createInputs(form, inputData);
+    });
+
+    if (formText.references) { createReference(form, formText); }
+
+    if (formText.buttons) { createButton(form, formText); }
+
+    formBlock.appendChild(form);
     return form;
 }
-async function allForms() {
-    await createFormAddPost();
-    const postForm = document.querySelector("form#addpost");
-    postForm.addEventListener("submit", async (e) => {
+async function forms(fileUrl) {
+    const text = await readTextFile(fileUrl);
+    let formText = JSON.parse(text);
+    await createForm(formText);
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let postFormData = Array.from(postForm.querySelectorAll('input')).reduce((acc, input) => ({
+        let formData = Array.from(form.querySelectorAll('input')).reduce((acc, input) => ({
             ...acc,
             [input.id]: input.value
         }), {});
+        /*
         const response = await fetch("storage/posts.json", {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(postFormData)
+            body: JSON.stringify(formData)
         })
         console.log(response);
-        console.log(JSON.stringify(postFormData));
+        */
+        console.log(JSON.stringify(formData));
     })
 }
-allForms();
+forms("./schemas/interview.json");
---
+++ scripts/inputFunctions.js
@@ -0,0 +1,87 @@
+function createLabel(inputData) {
+    let inputLabel = document.createElement('label');
+    inputLabel.innerHTML = inputData.label;
+    inputLabel.setAttribute('for', inputData.label );
+    return inputLabel;
+}
+function simpleInput(form, inputData, needLabel = false) {
+    let input = document.createElement('input');
+    input.setAttribute('type', inputData.input.type );
+    input.setAttribute('name', inputData.label );
+    input.setAttribute('id', inputData.label );
+    const inputLabel = createLabel(inputData);
+    form.appendChild(inputLabel);
+    form.appendChild(input);
+    if (needLabel) { return [input, inputLabel]; }
+    return input;
+}
+function textareaInput(form, inputData) {
+    let inputTextarea = document.createElement('textarea');
+    const input = simpleInput(form, inputData);
+    form.replaceChild(inputTextarea, input);
+    inputTextarea.setAttribute('name', inputData.label );
+    inputTextarea.setAttribute('id', inputData.label );
+    inputTextarea.setAttribute('rows', '2' );
+    return inputTextarea;
+}
+function checkboxInput(form, inputData) {
+    const checkboxArr = simpleInput(form, inputData, true);
+    const CheckboxBlock = document.createElement('div');
+    CheckboxBlock.classList.add('checkbox-block');
+    checkboxArr.forEach((el) => {
+        el.parentNode.insertBefore(CheckboxBlock, el);
+        CheckboxBlock.appendChild(el);
+    })
+    return checkboxArr[0];
+}
+function colorInputs(form, inputData) {
+    inputData.input.colors.forEach((color) =>{
+        let element = document.createElement('input');
+        let elementLabel = document.createElement('label');
+        let elementBlock = document.createElement('div');
+        element.setAttribute('type', 'checkbox');
+        elementLabel.setAttribute('style', `background-color: ${color}`);
+        element.setAttribute('id', color);
+        element.setAttribute('name', color);
+        elementLabel.setAttribute('for', color);
+        elementBlock.classList.add(`color-block`);
+        elementBlock.appendChild(element);
+        elementBlock.appendChild(elementLabel);
+        form.appendChild(elementBlock);
+    });
+}
+function technologyInputs(form, inputData) {
+    let elementLabel = document.createElement('label');
+    let elementBlock = document.createElement('select');
+    elementLabel.innerHTML = inputData.label;
+    let standardOption = document.createElement('option');
+    elementBlock.setAttribute('multiple', 'true');
+    elementBlock.setAttribute('size', '5');
+
+    standardOption.innerHTML = "choose";
+    elementBlock.appendChild(standardOption);
+    inputData.input.technologies.forEach((technology) => {
+        let element = document.createElement('option');
+        element.innerHTML = technology;
+        element.setAttribute('value', technology);
+        elementBlock.appendChild(element);
+    });
+
+    form.appendChild(elementLabel);
+    form.appendChild(elementBlock);
+    return elementBlock;
+}
+function fileInput(form, inputData) {
+    const input = simpleInput(form, inputData);
+    input.setAttribute('multiple', 'true')
+    if (inputData.input.filetype) {
+        let acceptFiles = "";
+        let i =0;
+        inputData.input.filetype.forEach((type) => {
+            acceptFiles += (i === 0) ? "." + type : ",." + type;
+            i++;
+        });
+        input.setAttribute('accept', acceptFiles);
+    }
+    return input;
+}
